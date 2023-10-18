9493a7dcdb30675668bcf30365376739af0af1c0
Martin Laurence Coloma <cmartinlaurence@gmail.com>
2023-10-18 18:38:13 +0800

    ENR v.1.0.0

--- EatNRunProject/EatnRun.Designer.cs
+++ EatNRunProject/EatnRun.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UserPickerPanel = new System.Windows.Forms.Panel();
+            this.CashierPB = new System.Windows.Forms.PictureBox();
+            this.ManagerPB = new System.Windows.Forms.PictureBox();
+            this.AdminPB = new System.Windows.Forms.PictureBox();
+            this.ENRLogo1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.UserPickerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CashierPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ManagerPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AdminPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ENRLogo1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UserPickerPanel
+            // 
+            this.UserPickerPanel.Controls.Add(this.panel1);
+            this.UserPickerPanel.Controls.Add(this.CashierPB);
+            this.UserPickerPanel.Controls.Add(this.ManagerPB);
+            this.UserPickerPanel.Controls.Add(this.AdminPB);
+            this.UserPickerPanel.Controls.Add(this.ENRLogo1);
+            this.UserPickerPanel.Location = new System.Drawing.Point(103, -1);
+            this.UserPickerPanel.Name = "UserPickerPanel";
+            this.UserPickerPanel.Size = new System.Drawing.Size(498, 455);
+            this.UserPickerPanel.TabIndex = 0;
+            // 
+            // CashierPB
+            // 
+            this.CashierPB.Image = global::EatNRunProject.Properties.Resources.Cashier;
+            this.CashierPB.Location = new System.Drawing.Point(337, 163);
+            this.CashierPB.Name = "CashierPB";
+            this.CashierPB.Size = new System.Drawing.Size(128, 128);
+            this.CashierPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.CashierPB.TabIndex = 3;
+            this.CashierPB.TabStop = false;
+            // 
+            // ManagerPB
+            // 
+            this.ManagerPB.Image = global::EatNRunProject.Properties.Resources.Manager;
+            this.ManagerPB.Location = new System.Drawing.Point(182, 163);
+            this.ManagerPB.Name = "ManagerPB";
+            this.ManagerPB.Size = new System.Drawing.Size(128, 128);
+            this.ManagerPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ManagerPB.TabIndex = 2;
+            this.ManagerPB.TabStop = false;
+            // 
+            // AdminPB
+            // 
+            this.AdminPB.Image = global::EatNRunProject.Properties.Resources.Admin;
+            this.AdminPB.Location = new System.Drawing.Point(33, 163);
+            this.AdminPB.Name = "AdminPB";
+            this.AdminPB.Size = new System.Drawing.Size(128, 128);
+            this.AdminPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.AdminPB.TabIndex = 1;
+            this.AdminPB.TabStop = false;
+            // 
+            // ENRLogo1
+            // 
+            this.ENRLogo1.Image = global::EatNRunProject.Properties.Resources.AP_Logo_128x128;
+            this.ENRLogo1.Location = new System.Drawing.Point(184, 0);
+            this.ENRLogo1.Name = "ENRLogo1";
+            this.ENRLogo1.Size = new System.Drawing.Size(128, 128);
+            this.ENRLogo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ENRLogo1.TabIndex = 0;
+            this.ENRLogo1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 455);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::EatNRunProject.Properties.Resources.AP_Logo_128x128;
+            this.pictureBox4.Location = new System.Drawing.Point(184, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // EatnRun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(51)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(704, 461);
+            this.Controls.Add(this.UserPickerPanel);
+            this.Name = "EatnRun";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.UserPickerPanel.ResumeLayout(false);
+            this.UserPickerPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CashierPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ManagerPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AdminPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ENRLogo1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel UserPickerPanel;
+        private System.Windows.Forms.PictureBox ENRLogo1;
+        private System.Windows.Forms.PictureBox CashierPB;
+        private System.Windows.Forms.PictureBox ManagerPB;
+        private System.Windows.Forms.PictureBox AdminPB;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
